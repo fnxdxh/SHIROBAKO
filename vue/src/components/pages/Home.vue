ee2649edaf9165335889b051783f9f68d53ecb6f
StoppedRain-Z <zhangrt16@163.com>
2018-12-20 14:26:57 +0800

    changed

--- vue/src/components/pages/Home.vue
+++ vue/src/components/pages/Home.vue
@@ -2,10 +2,23 @@
   <div>
     <h1>HomePage</h1>
     <h1>{{$store.state.count}}</h1>
+    <h1>{{msg}}</h1>
   </div>
 </template>
 
 <script>
+export default{
+  data(){
+    return{
+      msg:{初始msg}
+    }
+  },
+  created(){
+    this.$http.get('url').then(result => {
+      this.msg = result.body
+    })
+  }
+}
 </script>
 
 <style scoped>
