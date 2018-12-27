9d57b021b2b1af27c869809cd42794a19d50cf3d
fnxdxh <fnxdxh@126.com>
2018-12-27 11:39:21 +0800

    修改matchlist

--- django/mysite/settings.py
+++ django/mysite/settings.py
@@ -112,7 +112,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'shirobako',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': '123dxh',
         'HOST': '127.0.0.1',
     }
 }
